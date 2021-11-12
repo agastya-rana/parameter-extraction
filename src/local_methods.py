b43f71fdfe8b7198945f6d18bf0854d676bc5382
Agastya Rana <agastya.rana@yale.edu>
2021-11-12 13:12:00 -0500

    Added local methods

---
+++ src/local_methods.py
@@ -0,0 +1,26 @@
+"""
+Miscellaneous functions that depend on the machine or local
+parameters or setups. 
+
+
+Created by Nirag Kadakia at 21:30 08-14-2017
+This work is licensed under the 
+Creative Commons Attribution-NonCommercial-ShareAlike 4.0 
+International License. 
+To view a copy of this license,
+visit http://creativecommons.org/licenses/by-nc-sa/4.0/.
+"""
+
+import sys, os
+
+scripts_path = [i for i in sys.path if 'scripts' in i][0]
+
+def def_data_dir():
+	"""
+	Define a data directory here for all 
+	scripts in this project
+	"""
+	
+	data_dir = os.path.join(os.path.dirname(scripts_path),'stim_select')
+	
+	return data_dir
